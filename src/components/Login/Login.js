308397f999f8daad37983649a7ff365293bbaed7
shiory602 <suzushioufo@gmail.com>
2021-11-10 14:48:47 -0800

    set login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+    <div className="login-main">
+      <div className="left">
+        <div className="logo-image">
+          <img src="" alt="logo" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+    </>
+  )
+}
+
+export default Login;
