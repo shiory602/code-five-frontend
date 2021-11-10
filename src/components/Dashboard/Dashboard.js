305596b93c866fe5a47b1c3ff4004f66faced949
shiory602 <suzushioufo@gmail.com>
2021-11-10 14:35:56 -0800

    first setting

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <>
+      <h1>dashboard page</h1>
+    </>
+  )
+}
+
+export default Dashboard;
---
+++ src/router/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Dashboard from "../components/Dashboard/Dashboard.js";
+// import Login from "../components/Login/Login";
+// import Wrapper from "../components/Wrapper/Wrapper";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+const AppRouter = () => (
+  <Router history={history}>
+    {/* <Wrapper> */}
+      <Switch>
+        <Route path="/" component={Dashboard} exact={true} />
+        {/* <Route path="/login" component={Login} /> */}
+      </Switch>
+    {/* </Wrapper> */}
+  </Router>
+);
+
+export { history, AppRouter as default };
