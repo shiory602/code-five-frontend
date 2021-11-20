2d330445824c194a9bab5eca16357d159a95434c
Guilherme Gianjope <guigianjope@gmail.com>
2021-11-19 18:16:06 -0800

    settings page

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import "../../scss-config/material-ui.scss";
+import "./Settings.css";
 
 const Settings = () => {
     return (
       <div>
         <Navbar />
-        Settings
-      </div>
+        <div className="settings-container">
+            <div className="settings-box">
+                <div className="settings-page">
+                    <h3>Settings</h3>
+
+                    
+                    <div className="buttonContainer">
+                        <button className="button" variant="contained">Dashboard...</button>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )}  
 
 export default Settings;
